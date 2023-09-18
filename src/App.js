691e1fb186db17d49164ff478ec21e32581ea978
Parag <paragpurandare50@gmail.com>
2023-09-18 11:12:36 +0530

    added the frontend files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Board from './components/Board';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Board />} />
-        {/* Add additional routes for instructions, about, etc. */}
-      </Routes>
-    </Router>
-  );
+    const [n, setN] = useState(8);
+    const [solutions, setSolutions] = useState([]);
+    const [isSolved, setIsSolved] = useState(false);
+
+    const handleSizeChange = (event) => {
+        setN(event.target.value);
+    };
+
+    const solvePuzzle = async () => {
+        try {
+            const response = await axios.post('/solveNQueens', { n });
+            setSolutions(response.data.solutions);
+            setIsSolved(true);
+        } catch (error) {
+            console.error('Error solving N-Queens:', error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>N-Queens Puzzle Solver</h1>
+            <div className="board">
+                <label>
+                    Board Size:
+                    <input type="number" value={n} onChange={handleSizeChange} min="1" />
+                </label>
+                <button onClick={solvePuzzle}>Solve</button>
+                {isSolved ? (
+                    <div className="solutions">
+                        <h2>Solutions:</h2>
+                        {solutions.map((solution, index) => (
+                            <div key={index} className="solution">
+                                {solution}
+                            </div>
+                        ))}
+                    </div>
+                ) : null}
+            </div>
+        </div>
+    );
 }
 
 export default App;
